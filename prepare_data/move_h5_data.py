285e6a39f89ba34e6b0c79b3e643f3b14327d87c
Amos Rada <adrada1213@gmail.com>
2018-12-14 17:31:51 +1300

    move_h5_data changes
    + Put comments
    + Utilise slice count to access only a small amount from data (faster reading)
    + Fix some bugs

--- prepare_data/move_h5_data.py
+++ prepare_data/move_h5_data.py
@@ -7,7 +7,18 @@ from prepare_h5_files import add_datasets, create_datasets, DataSetModel
 from datetime import datetime
 from time import time
 
+'''
+This function calculates the current division of data (test, train, validate) and the total
+number of slices
+Input:  h5_file = path to the h5 file
 
+Output: curr_test = current number of slices for the test group
+        curr_train = current number of slices for the train group
+        curr_val = current number of slices for the validation group
+        total = total number of slices
+
+TODO: Need to change the way I read the groups for efficiency
+'''
 def get_curent_ratio(h5_file):
     total = 0
     with h5py.File(h5_file, "r") as hf:
@@ -19,6 +30,15 @@ def get_curent_ratio(h5_file):
 
     return curr_test, curr_train, curr_val, total
 
+'''
+This function calculates the desired division of data (test, train, validate)
+Input:  Total = total number of slices
+        ratio = expected ratio
+
+Output: test_target = target number of slices for the test group
+        train_target = target number of slices for the train group
+        val_target = target number of slices for the validation group
+'''
 def calculate_targets(total, ratio):
     test_target = int(total*ratio[0])
     train_target = int(total*ratio[1])
@@ -26,7 +46,21 @@ def calculate_targets(total, ratio):
 
     return test_target, train_target, val_target
 
-# if the difference is negative, this means that the current number of slices in that data is more than the desired number of slices for that data
+'''
+This function calculates the difference between the desired number of slices and the current number of slices for each group.
+(If the difference is negative, this means that the current number of slices in that data is more than the desired number of 
+slices for that data)
+Input:  curr_test = current number of slices for the test group
+        curr_train = current number of slices for the train group
+        curr_val = current number of slices for the validation group
+        test_target = target number of slices for the test group
+        train_target = target number of slices for the train group
+        val_target = target number of slices for the validation group
+
+Output: test_diff = difference for test group
+        train_diff = difference for train group
+        val_diff = difference for validation group
+'''
 def calculate_differences(curr_test, curr_train, curr_val, trgt_test, trgt_train, trgt_val):
     test_diff = trgt_test-curr_test
     train_diff = trgt_train-curr_train
@@ -34,13 +68,44 @@ def calculate_differences(curr_test, curr_train, curr_val, trgt_test, trgt_train
 
     return test_diff, train_diff, val_diff
 
+
+'''
+This function calculates the number of slices for each group
+'''
 def get_slice_count(h5_file, group):
     with h5py.File(h5_file, "r") as hf:
         slice_count = np.array(hf["/{}/patients".format(group)]).shape[0]
     
     return slice_count
 
-def get_data_from_group(dsm, h5_file, group, index):
+'''
+This function adds the data needed to the model
+'''
+def add_data_to_model(dsm, grp, cine_grp, tagged_grp, start, end):
+    dsm.patient_names = np.array(grp.get("patients")[start:end])
+    dsm.slices = np.array(grp.get("slices")[start:end]) 
+    dsm.landmark_coords = np.array(grp.get("landmark_coords")[start:end,:,:,:])
+    dsm.bbox_corners = np.array(grp.get("bbox_corners")[start:end,:])
+    dsm.cim_paths = np.array(grp.get("cim_paths")[start:end])
+
+    # cines
+    dsm.cine_dicom_paths = np.array(cine_grp.get("cine_dicom_paths")[start:end, :])
+    dsm.cine_img_orient = np.array(cine_grp.get("cine_image_orientations")[start:end,:])
+    dsm.cine_img_pos = np.array(cine_grp.get("cine_image_positions")[start:end,:])
+    dsm.cine_images = np.array(cine_grp.get("cine_images")[start:end,:,:,:])
+    dsm.cine_px_spaces = np.array(cine_grp.get("cine_px_spaces")[start:end])
+
+    # tagged
+    dsm.tagged_dicom_paths = np.array(tagged_grp.get("tagged_dicom_paths")[start:end, :])
+    dsm.tagged_img_orient = np.array(tagged_grp.get("tagged_image_orientations")[start:end,:])
+    dsm.tagged_img_pos = np.array(tagged_grp.get("tagged_image_positions")[start:end,:])
+    dsm.tagged_images = np.array(tagged_grp.get("tagged_images")[start:end,:,:,:])
+    dsm.tagged_px_spaces = np.array(tagged_grp.get("tagged_px_spaces")[start:end])
+
+'''
+
+'''
+def get_data_from_group(dsm, h5_file, group, index, slice_count):
     with h5py.File(h5_file, "r") as hf:
         # get the groups needed
         grp = hf.get(group)
@@ -48,94 +113,86 @@ def get_data_from_group(dsm, h5_file, group, index):
         tagged_grp = grp.get("tagged")
 
         # get the list of patients
-        patients = np.array(grp.get("patients"))
-        patient = patients[-index] #take only one patient
+        try:
+            patients = np.array(grp.get("patients")[slice_count-5:slice_count])   #so we don't have to read the entire patient array
+        except IndexError:
+            patients = np.array(grp.get("patients")[:slice_count])
+        patient = patients[-1] #take only one patient
         p_indices = np.array(np.where(patients==patient))[0]    #get the indices of this patient in the h5 file
-        start, end = p_indices[0], p_indices[-1]+1  #start and end indices
-        dsm.patient_names = patients[start:end]
-        dsm.slices = np.array(grp.get("slices"))[start:end] 
-        dsm.landmark_coords = np.array(grp.get("landmark_coords"))[start:end,:,:,:]
-        dsm.bbox_corners = np.array(grp.get("bbox_corners"))[start:end,:]
-        dsm.cim_paths = np.array(grp.get("cim_paths"))[start:end]
+        start, end = slice_count-len(p_indices), slice_count  #start and end indices
+        add_data_to_model(dsm, grp, cine_grp, tagged_grp, start, end)
 
-        # cines
-        dsm.cine_dicom_paths = np.array(cine_grp.get("cine_dicom_paths"))[start:end, :]
-        dsm.cine_img_orient = np.array(cine_grp.get("cine_image_orientations"))[start:end,:]
-        dsm.cine_img_pos = np.array(cine_grp.get("cine_image_positions"))[start:end,:]
-        dsm.cine_images = np.array(cine_grp.get("cine_images"))[start:end,:,:,:]
-        dsm.cine_px_spaces = np.array(cine_grp.get("cine_px_spaces"))[start:end] 
-
-        # tagged
-        dsm.tagged_dicom_paths = np.array(tagged_grp.get("tagged_dicom_paths"))[start:end, :]
-        dsm.tagged_img_orient = np.array(tagged_grp.get("tagged_image_orientations"))[start:end,:]
-        dsm.tagged_img_pos = np.array(tagged_grp.get("tagged_image_positions"))[start:end,:]
-        dsm.tagged_images = np.array(tagged_grp.get("tagged_images"))[start:end,:,:,:]
-        dsm.tagged_px_spaces = np.array(tagged_grp.get("tagged_px_spaces"))[start:end] 
-    
     return index+len(p_indices)
 
+'''
+This function creates the group in the new h5 file
+Input:  dsm = DataSetModel containing data to be added
+        new_h5_file = path to the new h5 file
+        group = group we're adding
+'''
 def create_group(dsm, new_h5_file, group):
     with h5py.File(new_h5_file, "a") as hf:
         create_datasets(hf, group, dsm)
 
-def add_data_to_group(dsm, h5_file, group):
-    with h5py.File(h5_file, "a") as hf:
+'''
+This adds data to the new h5 file
+Input:  dsm = DataSetModel containing data to be added
+        new_h5_file = path to the new h5 file
+        group = group to be added
+'''
+def add_data_to_group(dsm, new_h5_file, group):
+    with h5py.File(new_h5_file, "a") as hf:
         add_datasets(hf, group, dsm)
 
-def get_data_for_group(h5_file, group, start, end):
-    dsm = DataSetModel()
+'''
+This reads the old h5 file and gets the data for all the slices for the current patient
+Input:  h5_file = old h5 file path
+        group = group that we're copying 
+        start = index of the first slice for the current patient
+        end = index of the last slice for the current patient
+'''
+def get_data_for_group(dsm, h5_file, group, start, end):
     with h5py.File(h5_file, "r") as hf:
         # get the groups needed
         grp = hf.get(group)
         cine_grp = grp.get("cine")
         tagged_grp = grp.get("tagged")
 
-        # get the list of patients
-        dsm.patient_names = np.array(grp.get("patients"))[start:end]
-        dsm.slices = np.array(grp.get("slices"))[start:end] 
-        dsm.landmark_coords = np.array(grp.get("landmark_coords"))[start:end,:,:,:]
-        dsm.bbox_corners = np.array(grp.get("bbox_corners"))[start:end,:]
-        dsm.cim_paths = np.array(grp.get("cim_paths"))[start:end]
-
-        # cines
-        dsm.cine_dicom_paths = np.array(cine_grp.get("cine_dicom_paths"))[start:end, :]
-        dsm.cine_img_orient = np.array(cine_grp.get("cine_image_orientations"))[start:end,:]
-        dsm.cine_img_pos = np.array(cine_grp.get("cine_image_positions"))[start:end,:]
-        dsm.cine_images = np.array(cine_grp.get("cine_images"))[start:end,:,:,:]
-        dsm.cine_px_spaces = np.array(cine_grp.get("cine_px_spaces"))[start:end] 
-
-        # tagged
-        dsm.tagged_dicom_paths = np.array(tagged_grp.get("tagged_dicom_paths"))[start:end, :]
-        dsm.tagged_img_orient = np.array(tagged_grp.get("tagged_image_orientations"))[start:end,:]
-        dsm.tagged_img_pos = np.array(tagged_grp.get("tagged_image_positions"))[start:end,:]
-        dsm.tagged_images = np.array(tagged_grp.get("tagged_images"))[start:end,:,:,:]
-        dsm.tagged_px_spaces = np.array(tagged_grp.get("tagged_px_spaces"))[start:end] 
+        add_data_to_model(dsm, grp, cine_grp, tagged_grp, start, end)
     
-    return dsm
-
+'''
+This function copies h5 data from old h5 file to the new h5 file
+Input: h5_file = path to the old h5 file
+        new_h5_file = path to the new h5 file
+        group = group we're copying
+        init = whether the h5 file is already created or not
+        limit = index of the last slice we want to add (from the right)
+'''
 def copy_h5_data(h5_file, new_h5_file, group, init, limit):
-    if init:
+    if init:    #to create the file
         hf = h5py.File(new_h5_file, "w")
         hf.close()
 
+    # read the new h5 file and determine whether the group exists
     with h5py.File(new_h5_file, "r") as hf:
         if "/{}".format(group) not in hf:
             init_group = True
         else:
             init_group = False
     
-    slice_cnt = get_slice_count(h5_file, group)
-    if limit is not None:
+    slice_cnt = get_slice_count(h5_file, group) #get the slice count
+    if limit is not None:   #if the last index(from the right) is specified
         slice_cnt = slice_cnt - (limit-1)
     for i in range(0,slice_cnt,5):    #loop through the data in small increments to not overload the memory(if we have huge h5 files)
+        dsm = DataSetModel()
         if (slice_cnt - i) < 5:   #if we've reached the end of the data
-            dsm = get_data_for_group(h5_file, group, i, slice_cnt)
+            get_data_for_group(dsm, h5_file, group, i, slice_cnt)  #getting data for the group
         else:
-            dsm = get_data_for_group(h5_file, group, i, i+5)
-        if init_group:
+            get_data_for_group(dsm, h5_file, group, i, i+5)
+        if init_group:  #create group if it hasn't been created
             create_group(dsm, new_h5_file, group)
             init_group = False
-        else:
+        else:   #otherwise just append data to the group
             add_data_to_group(dsm, new_h5_file, group)
 
     return
@@ -144,7 +201,7 @@ def copy_h5_data(h5_file, new_h5_file, group, init, limit):
 This is the main function
 Inputs: h5_file = path to the h5 file we want to modify
         new_h5_file = path to the new h5 file we want to create
-        ratio = test:train:validate ratio as a list 
+        ratio = test:train:validation ratio as a list 
 
 Overview:
     1. Calculates the difference between the target number of slices and the current number of slices for each group
@@ -169,9 +226,8 @@ How data is moved/copied:
         -Move on to the next group
 
     Second case(Adding data to 2 groups from 1 group):
-    Difference from the first case: We're copying the group we're getting 
-    
-
+    Difference from the first case: Copy from both of the groups we're adding data to to the new h5 file
+    -Once all target number of slices met, we copy data from the group we're getting data from (only up until the end index)
 '''
 def move_h5_data(h5_file, new_h5_file, ratio):
     # groups in the h5 file
@@ -209,13 +265,15 @@ def move_h5_data(h5_file, new_h5_file, ratio):
         init = False
         # loop through the groups that we're taking data from
         for grp in grp_from:
+            print("From {} to {}".format(grp, grp_to[0]))
             #index keeps track of the number of data we're copying from the from group in the old h5 file to the group in the new h5 file
             index = 1   
             slice_cnt = get_slice_count(h5_file, grp)   #get the current number of slices
             # loop until the number of slices is equal to or less than the target number of slices for that group
             while slice_cnt > trgt_dict[grp]:
                 dsm = DataSetModel()    #initiate the model
-                index = get_data_from_group(dsm, h5_file, grp, index)   #get data from group from old h5 file
+                index = get_data_from_group(dsm, h5_file, grp, index, slice_cnt)   #get data from group from old h5 file
+                print("Moving {} slices for patient {}".format(len(dsm.slices), dsm.patient_names[0]))
                 add_data_to_group(dsm, new_h5_file, grp_to[0])  #add data to new h5 file
                 slice_cnt -= len(dsm.slices)    #update the number of slices of the group we're taking data from
             copy_h5_data(h5_file, new_h5_file, grp, init, index)    #if target number of slices is reached, we copy the data from the current group to the new h5 file
@@ -227,17 +285,21 @@ def move_h5_data(h5_file, new_h5_file, ratio):
         copy_h5_data(h5_file, new_h5_file, grp_to[0], init, None)
         init = False
         copy_h5_data(h5_file, new_h5_file, grp_to[1], init, None)
+        #index keeps track of the number of data we're copying from the from group in the old h5 file to the group in the new h5 file
+        index = 1
+        slice_cnt_from = get_slice_count(h5_file, grp_from[0])   #get the current number of slices
         # loop through the groups we're adding data to
         for grp in grp_to:
-            #index keeps track of the number of data we're copying from the from group in the old h5 file to the group in the new h5 file
-            index = 1
+            print("From {} to {}".format(grp_from[0], grp))
             slice_cnt = get_slice_count(h5_file, grp)   #get the current number of slices
             # loop until we get the desired number of slices
             while slice_cnt < trgt_dict[grp]:   #if the number of slices for the current group reaches the target, we move on to the next group
                 dsm = DataSetModel()     #initiate the model
-                index = get_data_from_group(dsm, h5_file, grp_from[0], index)  
+                index = get_data_from_group(dsm, h5_file, grp_from[0], index, slice_cnt_from)
+                print("Moving {} slices for patient {}".format(len(dsm.slices), dsm.patient_names[0]))  
                 add_data_to_group(dsm, new_h5_file, grp)    #add data to new h5 file
                 slice_cnt += len(dsm.slices)    #update the number of slices
+                slice_cnt_from -= len(dsm.slices)
         #if target number of slices is reached, we copy the data from the current group to the new h5 file
         copy_h5_data(h5_file, new_h5_file, grp_from[0], init, index)    
 
@@ -254,7 +316,8 @@ def move_h5_data(h5_file, new_h5_file, ratio):
         slice_cnt = get_slice_count(h5_file, grp_from[0])
         while slice_cnt > trgt_dict[grp_from[0]]:
             dsm = DataSetModel()
-            index = get_data_from_group(dsm, h5_file, grp_from[0], index)
+            index = get_data_from_group(dsm, h5_file, grp_from[0], index, slice_cnt)
+            print("Moving {} slices for patient {}".format(len(dsm.slices), dsm.patient_names[0]))
             add_data_to_group(dsm, new_h5_file, grp_to[0])
             slice_cnt -= len(dsm.slices)
         copy_h5_data(h5_file, new_h5_file, grp_from[0], init, index)
@@ -274,11 +337,11 @@ if __name__ == "__main__":
     new_h5_filename = "UK_Biobank_50cases_new.h5"   #specify the new filename
     new_h5_file = os.path.join(output_dir, new_h5_filename)
 
-    # specify how you want to divide the data [test, train, validation]
-    ratio = [0.2, 0.6, 0.2]
+    # specify how you want to divide the data [test, train, validation], total must equal to 1
+    ratio = [0.1, 0.8, 0.1]
 
     # change to False if you don't want to keep the old h5 file
-    del_old = True
+    del_old = False
     print("Moving h5 data from {} to {}".format(os.path.basename(h5_file), os.path.basename(new_h5_file)))
     print("h5 files in {}".format(os.path.dirname(h5_file)))
 
